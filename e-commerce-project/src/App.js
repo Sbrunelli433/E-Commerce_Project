60ff674cbf1e1f028746742800046c757da22479
Sbrunelli433 <brunes46@gmail.com>
2020-03-30 14:07:39 -0500

    created sign-in page with a form. No functionality or style yet.

--- e-commerce-project/src/App.js
+++ e-commerce-project/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import Header from './components/header/header.component.jsx';
 
 /*use <Switch> to have more control over the code and where the user can go and if only one route
@@ -15,6 +16,7 @@ function App(){
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route exact path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInAndSignUpPage}/>
    
     </Switch>
     </div>
