313e40cbbf2e450786e56fa191caab1a910bee5e
Sbrunelli433 <brunes46@gmail.com>
2020-03-30 14:28:25 -0500

    created form-input component and formatted the sign-in page

--- e-commerce-project/src/components/sign-in/sign-in.component.jsx
+++ e-commerce-project/src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component.jsx';
+
 import './sign-in.styles.scss';
 
 //class component to store information
@@ -36,16 +38,22 @@ class SignIn extends React.Component{
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input name="email" type="email" onChange={this.handleChange} value={this.state.email} required />
-                        <label>Email</label>
-                    <input 
+                    <FormInput 
+                        name="email" 
+                        type="email" 
+                        handleChange={this.handleChange} 
+                        value={this.state.email} 
+                        label="email"
+                        required 
+                    />
+                    <FormInput 
                         name="password" 
                         type="password" 
                         value={this.state.email} 
-                        onChange={this.handleChange}
+                        handleChange={this.handleChange}
+                        label="password"
                         required
                         />
-                        <label>Password</label>
                     <input type='submit' value='Submit Form' />
                 </form>
             </div>
