30fd0595a25e88ff9d761de32c681a1539f52c57
Sbrunelli433 <brunes46@gmail.com>
2020-04-01 15:45:18 -0500

    created array in the cart to store items when addItem is clicked. Cart component visually is still empty, but the item is appended to the array

--- e-commerce-project/src/redux/cart/cart.actions.js
+++ e-commerce-project/src/redux/cart/cart.actions.js
@@ -3,3 +3,12 @@ import CartActionTypes from './cart.types';
 export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
+
+/* Actions can have a type value and a payload value
+payload value can be anything. In this case, it is the item
+we want to add to our array 
+this is called in the collection-item.component.jsx to add items to the cart*/
+export const addItem= item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+})
--- e-commerce-project/src/redux/cart/cart.reducer.js
+++ e-commerce-project/src/redux/cart/cart.reducer.js
@@ -1,9 +1,9 @@
 import CartActionTypes from './cart.types';
 
-
 //hidden value is true because we want to hide the cart dropdown when the user first comes to the site
-const INITIAL_STATE ={
-    hidden: true
+const INITIAL_STATE = {
+    hidden: true,
+    cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -12,10 +12,17 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 hidden: !state.hidden
+            };
+        case CartActionTypes.ADD_ITEM:
+            return{
+                ...state,
+                /*BELOW: spreading in all array values, adding additional values will appear at the
+                end of this array following the spread values from existing array*/
+                cartItems:[...state.cartItems, action.payload]
             }
             default: 
-            return state;
+                return state;
         }
-};
+    };
 
 export default cartReducer;
