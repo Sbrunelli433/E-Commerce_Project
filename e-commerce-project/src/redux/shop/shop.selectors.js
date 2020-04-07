636e95f01fb2e1181ce44a5d18150850e7fa718b
Sbrunelli433 <brunes46@gmail.com>
2020-04-07 10:51:15 -0500

    normalized the shop.data.js from an array to an object. Modified the search function in shop.selectors.js to be more efficient and work with the list of objects that is now normalized.

--- e-commerce-project/src/redux/shop/shop.selectors.js
+++ e-commerce-project/src/redux/shop/shop.selectors.js
@@ -1,16 +1,16 @@
 import { createSelector } from 'reselect';
 
 //object where the string value goes to the id
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens:5
-}
+//only required when shop.data.js is still an array and not normalized
+// const COLLECTION_ID_MAP = {
+//     hats: 1,
+//     sneakers: 2,
+//     jackets: 3,
+//     womens: 4,
+//     mens:5
+// }
 
 const selectShop = state => state.shop;
- 
 
 export const selectCollections = createSelector(
     [selectShop],
@@ -22,10 +22,16 @@ export const selectCollections = createSelector(
 to createSelector as an array and put it through another function in collections.find() and compare the 
 COLLECTION_ID_MAP array and match it to the collectionUrlParam
 do this to create multiple utility functions*/
+// export const selectCollection = collectionUrlParam => 
+//     createSelector([selectCollections],
+//         collections => 
+//             collections.find(
+//                 collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+//             )
+// );
+
+/* to cut down on performance time, store the list of collections and items in an object: DATA NORMALIZATION */
 export const selectCollection = collectionUrlParam => 
     createSelector([selectCollections],
-        collections => 
-            collections.find(
-                collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-            )
-);
+        collections => collections[collectionUrlParam]
+    );
